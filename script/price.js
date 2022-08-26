f0e50b0556eea9f300efca15e80f58e0a2f59b78
ImDebabrata <debabratakgt@hotmail.com>
2022-08-27 01:55:20 +0530

    updated on day 4

--- script/price.js
+++ script/price.js
@@ -101,7 +101,7 @@ function plansTab() {
 
     if (el.button === "Try for free") {
       button.setAttribute("class", "try_free");
-      console.log(el);
+      // console.log(el);
       button.addEventListener("click", () => {
         displayPop(el.title);
       });
@@ -141,7 +141,7 @@ function plansTab() {
   let right_arrow = document.getElementById("right_arrow");
   right_arrow.addEventListener("click", () => {
     translatePos -= 24;
-    console.log("translatePos:", translatePos);
+    // console.log("translatePos:", translatePos);
     slider.style.transform = `translateX(${translatePos}vw)`;
     if (translatePos === -24) {
       right_arrow.style.display = "none";
@@ -152,7 +152,7 @@ function plansTab() {
   let left_arrow = document.getElementById("left_arrow");
   left_arrow.addEventListener("click", () => {
     translatePos += 24;
-    console.log("translatePos:", translatePos);
+    // console.log("translatePos:", translatePos);
     slider.style.transform = `translateX(${translatePos}vw)`;
     if (translatePos === 0) {
       left_arrow.style.display = "none";
@@ -264,6 +264,9 @@ function planFeature() {
     subtitle.innerText = el.subtitle;
     button.innerText = el.button;
     button.classList.add("try_free");
+    button.addEventListener("click", () => {
+      displayPop(el.title);
+    });
     div_plans.append(title, subtitle);
 
     el.features.forEach((item) => {
@@ -347,4 +350,207 @@ function featurePlans(arrow, feature_container) {
     plansClicked = true;
     feature_container.classList.add("hide_feature");
   }
+}
+
+//marketing tab
+marketingTab();
+function marketingTab() {
+  for (let i = 0; i < 2; i++) {
+    let commonTabBox = document.createElement("div");
+    let title = document.createElement("h4");
+    let subtitle = document.createElement("p");
+    let caption = document.createElement("p");
+    let contact = document.createElement("button");
+    let trybtn = document.createElement("button");
+    trybtn.classList.add("try_free");
+    commonTabBox.classList.add("commonTab_box");
+
+    if (i === 0) {
+      title.innerText = "Wrike for Marketing Teams";
+      subtitle.innerText =
+        "Powerful features custom-built for marketing and creative teams";
+      caption.innerText =
+        "Create high-quality, consistent brand experiences globally, at speed and scale.";
+
+      contact.innerText = "Contact us";
+      trybtn.innerText = "Try for free";
+
+      commonTabBox.append(title, subtitle, caption, contact, trybtn);
+
+      document.querySelector(".marketing_tab").append(commonTabBox);
+    } else {
+      title.innerText = "Wrike for Professional Services Teams";
+      subtitle.innerText =
+        "Powerful features custom-built for professional services teams";
+      caption.innerText =
+        "Stay on top of project delivery and turn billable time into revenue — fast.";
+
+      contact.innerText = "Contact us";
+      trybtn.innerText = "Try for free";
+
+      commonTabBox.append(title, subtitle, caption, contact, trybtn);
+
+      document.querySelector(".service_tab").append(commonTabBox);
+    }
+  }
+}
+
+//partner brands logo
+partnerBrands();
+function partnerBrands() {
+  logo_link = [
+    "https://web-static.wrike.com/content/uploads/2020/06/rebrand-logoroll_logo_google.svg?av=9b17bb4669ab35947f32929ac955bee2",
+    "https://web-static.wrike.com/content/uploads/2022/01/1440_logoroll_logo_syneos-1.svg?av=8245084e9f842e96e559e9a271044a88",
+    "https://web-static.wrike.com/content/uploads/2021/06/1440_logoroll_logo_lyft.svg?av=ff9ed8ad750c611fdf2e4594f05fa81b",
+    "https://web-static.wrike.com/content/uploads/2020/06/rebrand-logoroll_logo_Siemens.svg?av=e6aa94b50a6f43398434a97bbb106da8",
+    "https://web-static.wrike.com/content/uploads/2021/11/1440_logoroll_logo_nickelodeon.svg?av=a803611b33407188a89c536bc33e127f",
+    "https://web-static.wrike.com/content/uploads/2020/06/rebrand-logoroll_logo_Oglivy.svg?av=4eb15c17d1375900a7678e6cfcf07be9",
+    "https://web-static.wrike.com/content/uploads/2020/09/1440_logoroll_logo_snowflake.svg?av=7b1861c194a9ba9e67b2ff4a808b1db2",
+  ];
+
+  let logoDiv = document.createElement("div");
+  logo_link.forEach((el) => {
+    let image = document.createElement("img");
+    image.src = el;
+    logoDiv.append(image);
+  });
+  let text = document.createElement("div");
+  text.innerText = "TRUSTED BY 20,000+ ORGANIZATIONS WORLDWIDE";
+  document.querySelector(".brand_logo").append(logoDiv, text);
+}
+
+//addon feature;
+addOnFeature();
+function addOnFeature() {
+  let card_content = [
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_wrike-share.svg?av=58a3e7c51a1bcf7c50ee06223e32fe70",
+      hover_logo:
+        "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_wrike-share_inverse-1.svg?av=4c673fb2fec17b0103966d1144219365",
+      card_name: "Wrike Integrate",
+      text: "Create custom integrations and automations with 400+ cloud and on-premises applications.",
+      link: "Learn more",
+    },
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_two-way-sync.svg?av=823a9aebb7f1c14018fa6a7ae64fbed9",
+      hover_logo:
+        "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_two-way-sync_inverse.svg?av=3a118860b6be6ede7d061349ac2b1be8",
+      card_name: "Wrike Integrate",
+      text: "Create custom integrations and automations with 400+ cloud and on-premises applications.",
+      link: "Learn more",
+    },
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_lock.svg?av=e7277b0efdceebeafa444ac4e18ba90b",
+      hover_logo:
+        "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_lock_inverse.svg?av=e01395afa436ed913962b30e883e8647",
+      card_name: "Wrike Integrate",
+      text: "Create custom integrations and automations with 400+ cloud and on-premises applications.",
+      link: "Learn more",
+    },
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_manage-anything.svg?av=f29d1101c7c7bbeda975dc65bd0733c8",
+      hover_logo:
+        "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_manage-anything_inverse.svg?av=6ea4e5fd5077aaa5eba0ff2bd3ece847",
+      card_name: "Wrike Integrate",
+      text: "Create custom integrations and automations with 400+ cloud and on-premises applications.",
+      link: "Learn more",
+    },
+  ];
+
+  let addOnContainer = document.querySelector("#add_on_feature");
+  let heading = document.createElement("h2");
+  let subtitle = document.createElement("p");
+  let gridBox = document.createElement("div");
+  heading.classList.add("header_title", "sub__title");
+  subtitle.classList.add("header_subtitle", "sub__title");
+
+  heading.innerText = "Add-on Features";
+  subtitle.innerText =
+    "An add-on is a set of premium features that you can add to your Wrike account for an additional fee. Enhance your plan to unlock more value for your team.";
+
+  card_content.forEach((el) => {
+    let cardDiv = document.createElement("div");
+    let logoDiv = document.createElement("div");
+    let infoDiv = document.createElement("div");
+    let logo = document.createElement("img");
+    let hover_logo = document.createElement("img");
+    let card = document.createElement("h5");
+    card.classList.add("name_card");
+    let text = document.createElement("p");
+    text.classList.add("card_text");
+    let link = document.createElement("p");
+
+    logo.src = el.logo;
+    hover_logo.src = el.hover_logo;
+    card.innerText = el.card_name;
+    text.innerText = el.text;
+    link.innerText = el.link;
+
+    hover_logo.classList.add("hover_effect");
+    cardDiv.classList.add("hover_effect");
+
+    logoDiv.append(logo, hover_logo);
+    infoDiv.append(card, text, link);
+    cardDiv.append(logoDiv, infoDiv);
+    gridBox.append(cardDiv);
+  });
+
+  addOnContainer.append(heading, subtitle, gridBox);
+}
+
+//client success
+clientSection();
+function clientSection() {
+  let box_content = [
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_setting-gear.svg?av=0b708e74d43343f50156c3802e7b9f9a",
+      title: "Professional Services",
+      description:
+        "Wrike offers a selection of professional services packages to successfully onboard your team. Our consulting team will make sure you get the most out of your collaborative work management solution.",
+    },
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_collaboration.svg?av=8877e644b7c007a6e5e88d14673777b4",
+      title: "Customer Success Partnership",
+      description:
+        "Your dedicated advocate and trusted partner provides you with best practices and advice tailored to your goals and business objectives, ensuring your success with Wrike.",
+    },
+    {
+      logo: "https://web-static.wrike.com/content/uploads/2020/06/rebrand-icon_headphones.svg?av=fad9a561b19dd84be9eb855047ffcaa6",
+      title: "Global Support",
+      description:
+        "Wrike provides support through the Help Center, email, chat, and phone to address your questions and concerns. Get the support you need during your business hours, in your own language, the way you want.",
+    },
+  ];
+
+  let heading = document.createElement("h2");
+  let subtitle = document.createElement("p");
+  let boxContainer = document.createElement("div");
+  heading.classList.add("header_title", "sub__title");
+  subtitle.classList.add("header_subtitle", "sub__title");
+  boxContainer.classList.add("client_card_container");
+
+  heading.innerText = "Client Success with Wrike";
+  subtitle.innerText =
+    "Optimize Wrike to achieve your goals with expert consulting, training, and support.";
+
+  box_content.forEach((el) => {
+    let box = document.createElement("div");
+    let logo = document.createElement("img");
+    logo.src = el.logo;
+    let title = document.createElement("h5");
+    title.classList.add("name_card");
+    title.innerText = el.title;
+    let description = document.createElement("p");
+    description.classList.add("card_text");
+    description.innerText = el.description;
+
+    box.append(logo, title, description);
+    boxContainer.append(box);
+  });
+  let button = document.createElement("button");
+  button.innerText = "Contact us for pricing";
+
+  document
+    .querySelector("#client")
+    .append(heading, subtitle, boxContainer, button);
 }
