e35373c5671519f94e7cbb8be780ab76adbcdc13
gaurav78528 <gauravsuthar309@gmail.com>
2022-08-26 15:06:15 +0530

    added day3 work

--- script/helpcenter.js
+++ script/helpcenter.js
@@ -1,3 +1,45 @@
+let searchResult = document.getElementById("searchResult");
+let searchInput = document.getElementById("query").value;
+// let ul = document.getElementById("searchResult");
+let list = document.getElementById("unorderedList");
 
+async function search() {
+  try {
+    let res = await fetch(
+      `https://themealdb.com/api/json/v1/1/search.php?s=${searchInput}`
+    );
+    let data = await res.json();
+    let actual_data = data.meals;
+    console.log(actual_data);
+    localStorage.setItem("searchItem", JSON.stringify(actual_data));
+    append(actual_data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+let append = (data) => {
+  searchResult.innerHTML = "";
+  data.forEach((elem) => {
+    let list_item = document.createElement("li");
 
+    let link = document.createElement("a");
+    link.href = elem.strYoutube;
+    link.innerText = elem.strMeal;
+    // list_item.innerText = elem.strMeal;
+
+    list_item.append(link);
+    searchResult.append(list_item);
+    // ul.innerHTML = list_item;
+    // list.append(x);
+  });
+};
+
+function searchFun() {
+  // window.location.href = "search.html";
+  // localStorage.setItem("searchItem", JSON.stringify);
+}
+
+// if (searchInput !== "") {
+//   searchResult.style.display = "block";
+// }
--- script/ticket_form.js
+++ script/ticket_form.js
@@ -11,5 +11,11 @@ let isEmpty = () => {
 
   if (email != "" && subject != "" && desc != "" && options != "") {
     document.getElementById("submit_btn").removeAttribute("disabled");
+    document.getElementById("submit_btn").style.backgroundColor = "#017860";
+    document.getElementById("submit_btn").style.color = "white";
+    let submit_btn = document.getElementById("submit_btn");
+    submit_btn.addEventListener("click", function () {
+      alert("Submitted");
+    });
   }
 };
